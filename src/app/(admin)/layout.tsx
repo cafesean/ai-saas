77227f0571c80dd7534c8650a60f23a3f5ff9e6f
arthur.zou <arthur.zou@imaginato.com>
2025-03-27 11:59:29 +0800

    feat: Update model list page based on demo
    Signed-off-by: arthur.zou <arthur.zou@imaginato.com>

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -7,7 +7,7 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-100 flex h-full flex-col">
+    <div className="min-h-screen bg-white flex h-full flex-col">
       <NavMenu />
       <Sidebar />
       <main className="flex grow sm:ml-64 h-0 p-4 sm:p-4 lg:p-4 overflow-auto">
